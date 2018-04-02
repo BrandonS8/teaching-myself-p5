f67876cbe85007037ecfc8bc1b2765b268424a46
Brandon Sears <brandonsears8@gmail.com>
2018-04-02 11:26:41 -0400

    Revert "..."
    This reverts commit f41161645f0b08d74c486fc06a5fde1707dced66.

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,16 +1,18 @@
+
 function setup () {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(windowWidth, windowHeight);
   background(200, 200, 200);
+  // frameRate(400)
 }
 
 function draw () {
-  background(200, 200, 200);
-  if(mouseIsPressed){
+  if(mouseIsPressed) {
     addCircle(mouseX, mouseY);
   }
 }
 
 function addCircle(x,y){
   noStroke();
-  ellipse(x,y, 30, 30);
+  smooth();
+  ellipse(x, y, 20, 20);
 }
