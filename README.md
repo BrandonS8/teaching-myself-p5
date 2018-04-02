c41c611cf1917fd43597746a44f25c5fe0f5f956
Brandon Sears <brandonsears8@gmail.com>
2018-04-02 15:20:52 -0400

    setup div to show size and color instead of rectangle, add to readme

--- README.md
+++ README.md
@@ -1,11 +1,44 @@
+# Teaching Myself P5.js
+
+## Why I chose it
+
+I had seen some people on YouTube use it and it looked fun to play with the shapes and visual things it can do.
+
+## What I made
+I made a simple drawing application using p5.js for the first time. You can adjust the color and size of the brush as well as clear the screen.
 
 
+## Learning process
+I started by following the [getting started](https://p5js.org/get-started/) on the documentation, including their getting started download. That led to [the overview](https://github.com/processing/p5.js/wiki/p5.js-overview) which was nice but didn't explain a whole lot. After reading these I ventured into the [examples](https://p5js.org/examples/) to get some ideas of its use. I played around with a few of these and eventually determined I would build my own simple drawing app.
+
+With this idea in mind I started with using the documentation to make circles show up as I dragged my cursor. I got this to work but then discovered the pmouse doc. pmouse is basically the previous mouse position. I tried this out using lines with the mouse coordinates and the pmouse coordinates which worked beautifully compared to the circle method. This line way works a lot better and I later on discovered a few other examples online also use this technique.
+
+Most of my learning process was having an idea and looking through the documentation to figure out how to do it I had attempted to use google to find examples or answers but never found anything relevant.
 
 
-I started by following the [getting started](https://p5js.org/get-started/) on the documentation.
+## Possible Alternatives
+* [Anime.js](http://animejs.com/) (Bit more complicated and heavier focus on animation. Even if you aren't interested in Anime.js at least look at their website, it's pretty great.)
+* [Paper.js](http://paperjs.org/) (Pretty close to p5)
 
+
+## Problems with learning
+* Google is nearly useless when googling about p5 there is probably 25 sites mentioning p5.js. Google your problems with the term processing instead of p5 and try to figure out how to convert it into p5 is the best advice I can give.
+* Documentation is meh
+
+
+## What I think of it after using it
+
+I personally think it's pretty lacking and inefficient. A lot of things cannot be done without clearing the canvas then redrawing only what you want. It's nice for creating shapes and some 3D effects but there is surely better ways to do this out there. It seems pretty clear it's barely used after looking for any help online for it.
 
 
 ## Gotchas
-* You need semicolons
-* doesn't like es6
+
+* Have to clear canvas then redraw only what you want to get a lot of effects to work
+* The draw function is constantly looping unless you call noLoop()
+
+## Process for setting up p5 in general
+1. Just include p5 and any p5 libraries from a local or remote file
+```     <script src="../p5.min.js"></script>
+  //including p5 dom library
+    <script src="../addons/p5.dom.min.js"></script>
+    ```
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,44 +1,73 @@
-var r = 255;
-var g = 255;
-var b =  255;
+let r = 255;
+let g = 255;
+let b =  255;
+let size = 20;
 function setup () {
   // create canvas
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight)
   // sliders to control color
+  colorsLabel = createP('Colors (RGB)')
+  colorsLabel.position(windowWidth*0.85, 0)
   // red
-  sliderR = createSlider(0, 255, 0);
-  sliderR.position(windowWidth*0.85, 20);
+  sliderR = createSlider(0, 255, 0)
+  sliderR.position(windowWidth*0.85, 30)
   // green
-  sliderG = createSlider(0, 255, 0);
-  sliderG.position(windowWidth*0.85, 40);
+  sliderG = createSlider(0, 255, 0)
+  sliderG.position(windowWidth*0.85, 50)
   // blue
-  sliderB = createSlider(0, 255, 0);
-  sliderB.position(windowWidth*0.85, 60);
-  clearButton = createButton('CLEAR');
-  clearButton.position(windowWidth*0.885, 150);
-  clearButton.mouseClicked(clearDrawing);
-  frameRate(200);
+  sliderB = createSlider(0, 255, 0)
+  sliderB.position(windowWidth*0.85, 70)
+  // size slider
+  sizeLabel = createP('Size')
+  sizeLabel.position(windowWidth*0.85, 80)
+  sliderSize = createSlider(1, 255, 20)
+  sliderSize.position(windowWidth*0.85, 110)
+  // clear button
+  clearButton = createButton('CLEAR')
+  clearButton.position(windowWidth*0.885, 140)
+  clearButton.mouseClicked(clearDrawing)
+  // using div to show size and color so it changes without having to clear the canvas and redraw everytime it shrinks
+  div1 = createDiv('')
+  div1.position((windowWidth*0.79)-size, 150)
+  div1.style('display', 'flex')
+  div1.style('justify-content', 'center')
+  div1.style('align-items', 'center')
+  div2 = createDiv('')
+  div2.parent(div1)
+  div2.style('border-radius', '50%')
+  // line showing area u can draw in it disappears when you hit clear and adding this to draw doesn't fix....
+  line(windowWidth*0.82, 0, windowWidth*0.82, windowHeight)
+  // this makes the drawing a bit more accurate
+  frameRate(200)
 }
 
 function draw () {
-  r = sliderR.value();
-  g = sliderG.value();
-  b = sliderB.value();
-  fill(color(r,g,b));
-  noStroke();
-  rect(windowWidth*0.885, 80, 50, 50);
-  cursor(ARROW);
+  // slider values need to be used here because setup only runs once
+  r = sliderR.value()
+  g = sliderG.value()
+  b = sliderB.value()
+  size = sliderSize.value()
+  div1.size(300, 300)
+  // set size preview
+  div2.size(size, size)
+  // setting the color preview
+  div2.style('background', `rgb(${r}, ${g}, ${b})`)
+  // making cursor an arrow everywhere cuz it looks nice
+  cursor(ARROW)
 }
 
 function mouseDragged(){
-  if(mouseX < windowWidth*0.82){
-    smooth();
-    strokeWeight(20);
-    stroke(r, g, b);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+  // if the mouse or last mouse position is inside the area on the right side for the sliders then don't draw anything
+  // pmouse is the second to most recent mouse position
+  if(mouseX < windowWidth*0.81 && pmouseX < windowWidth*0.81){
+    smooth()
+    strokeWeight(size)
+    stroke(r, g, b)
+    line(mouseX, mouseY, pmouseX, pmouseY)
   }
 }
 
 function clearDrawing(){
-  clear();
+  // clears the canvas
+  clear()
 }
